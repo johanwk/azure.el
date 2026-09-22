0a88b5b3dcec173fc4b4f9b4c1a12379e0fc7934
johanwk <johan.w.kluwer@gmail.com>
2026-09-22 21:18:18 +0200

    make list-tests; run selected tests only

--- azure-devops-outline.el
+++ azure-devops-outline.el
@@ -5,25 +5,7 @@
 ;; Keywords: tools, azure, devops, outlines
 ;; Package-Requires: ((emacs "28.1") (azure-devops "2022.07.15"))
 
-;; Copyright (C) 2026 Henrik Kjerringvåg
-;;
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-;;; Commentary:
-;; Build a disposable, read-only Org snapshot of all work items in the current
-;; Azure DevOps project.  Parent/Child relations determine outline nesting;
-;; items with no Parent/Child relation are shown separately.
+;; <<license>>
 
 ;;; Code:
 
